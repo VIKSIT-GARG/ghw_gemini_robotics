ca0534a84ad1ce7b25aab0466ca967f2b04a0e24
Your Name <viksitg427@gmail.com>
2026-04-14 19:13:40 +0530

    changes_2

--- object_Detection.py
+++ object_Detection.py
@@ -1,6 +1,12 @@
 import os 
+from dotenv import load_dotenv   
 from google import genai
-from google.genai import types  
+from google.genai import types 
+import json 
+from PIL import Image 
+
+
+load_dotenv() 
 
 # step 1 - fetch out geminiapikey 
 
@@ -9,5 +15,35 @@ client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 #  loading image to prompt 
 
-image_path = "/home/byebye/Documents/MLH/robotics/robotics/image.png"
-my_image = client.files.upload(file-image_path)
+image_path = "image.png"
+my_image = client.files.upload(file=image_path)
+
+#step 3 - add a prompt 
+
+prompt = "locate the bee plushie from the image and give me the bounding box coordinates in JSON format [ymin,xmin,ymax,xmax] and also inclue a label "
+
+#step 4 - call the gemini robotics model - 
+
+image_response = client.models.generate_content(
+    model="gemini-robotics-er-1.5-preview",
+    contents=[
+        my_image,
+        prompt
+    ],
+    config = types.GenerateContentConfig(
+        temperature=0.5,
+        thinking_config=types.ThinkingConfig(thinking_budget=0)
+    )
+)
+
+print(image_response.text)
+
+
+
+#  step -5  parse the json respomse 
+
+data = json.loads(image_response.text)
+
+box = data[0]['box_2d']
+
+print(f"Bounding box coordinates for the bee plushie: {box}"   )
